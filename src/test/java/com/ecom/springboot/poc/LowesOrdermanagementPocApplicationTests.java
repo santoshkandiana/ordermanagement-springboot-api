3c693d0f0917f962743c3c453d20d4925fe9c707
z004g2f <santoshkumar.kandiana@target.com>
2020-04-29 07:55:56 -0500

    -Updated to naming convention for external poc projects.

--- src/test/java/com/ecom/springboot/poc/LowesOrdermanagementPocApplicationTests.java
+++ src/test/java/com/ecom/springboot/poc/LowesOrdermanagementPocApplicationTests.java
@@ -4,6 +4,6 @@ import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class LowesOrdermanagementPocApplicationTests {
+class EcomSpringBootPocApplicationTests {
 
 }
